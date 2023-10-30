f68b157ddb826748a0feb72de2e947a892d51a97
yan010508 <143437600+yan010508@users.noreply.github.com>
2023-10-30 14:49:27 -0300

    atualizando o arquivo.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("rodando o sistea de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("O Paula fez esse commit sem você ver ma aula")
-console.log("Enviandopara o Github")
+console.log("Enviando para o Github")
